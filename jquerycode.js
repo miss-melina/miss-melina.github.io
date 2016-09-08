1ef4728d5328d945b063b5c6eaf9ade9786f2b3e
miss-melina <melinamata49@gmail.com>
2016-09-08 16:41:35 -0700

    Update jquerycode.js

--- jquerycode.js
+++ jquerycode.js
@@ -5,8 +5,12 @@
 	*/
 	
 var main = function() {
-	alert("UNDER CONSTRUCTION");	
-	
+	/*alert("UNDER CONSTRUCTION");	*/
+	     $('.slide-left').click(function(){
+                //animate image position left
+                
+                $('img').animate({ left: '+=100px'},500);
+            });
 }
 
 $(document).ready(main);
